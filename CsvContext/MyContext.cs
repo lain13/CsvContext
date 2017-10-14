3e2afea7ae737fd3a3788e428aeb3ab00daff91a
KANG HYE WON <lain13@gmail.com>
2017-10-15 01:00:35 +0900

    초기 커밋.

---
+++ CsvContext/MyContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvContext
+{
+    public class MyContext
+    {
+        public IDbSet<Person> Persons { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ CsvContext/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MyContext();
+
+            var aaa = from c in context.Persons where c.Name == "KANG" select c;
+        }
+    }
+}
